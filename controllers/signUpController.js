396bcd386b80b58b16fbcfad2070621e117d0483
Shumaila <sayedshumaila27@gmail.com>
2025-07-27 21:31:47 +0530

    Sign up form setup and validation

--- controllers/signUpController.js
+++ controllers/signUpController.js
@@ -1,25 +1,65 @@
 const db = require('../db/queries');
+const { body, validationResult } = require('express-validator');
+
+const validateUser = [
+
+	body('fullname')
+		.trim()
+		.isLength({ min: 1, max: 20 })
+		.withMessage(`Full name must be between 1 and 20 characters.`),
+	body('username')
+		.trim()
+		.isLength({ min: 1, max: 10 })
+		.withMessage('Username must be between 1 and 10 characters.')
+		.custom(async (value) => {
+			const existingUser = await db.findByUsername(value);
+			if (existingUser) {
+				throw new Error('This username is taken');
+			}
+		}),
+	body('password')
+		.isLength({ min: 4 })
+		.withMessage('Password must be more than 4 characters.'),
+	body('confirmPassword')
+		.custom((value, { req }) => {
+			return value === req.body.password;
+		})
+		.withMessage('Password does not match.'),
+];
 
 const signUpForm = (req, res) => {
-    res.render('signUp')
-}
+	res.render('signUp');
+};
 
-const newUserPost = async (req, res) => {
-    try {
-        const [full_name, username, password] = req.user;
-        if (!full_name || !username || !password) {
-           res.status(404).send('User Inputs Not Found');
-            return;
+const newUserPost = [
+	validateUser,
+    async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).render('signUp', {
+				errors: errors.array(),
+			});
         }
+        
+        try {
+            console.log(req.body);
+            
+            const { fullname, username, password } = req.body;
+			if (!fullname || !username || !password) {
+				res.status(404).send('User Inputs Not Found');
+				return;
+			}
 
-        await db.newUserPost(full_name, username, password)
-        res.redirect('/')
-    } catch (error) {
-        console.log('Error creating user: ', error);
-		res.status(500).send('Internal Server Error');
-    }
-}
+			await db.newUserPost(fullname, username, password);
+			res.redirect('/');
+		} catch (error) {
+			console.log('Error creating user: ', error);
+			res.status(500).send('Internal Server Error');
+		}
+	},
+];
 
 module.exports = {
-    signUpForm, newUserPost
-}
+	signUpForm,
+	newUserPost,
+};
--- db/queries.js
+++ db/queries.js
@@ -1,11 +1,21 @@
 const pool = require('./pool');
 
-async function newUserPost(full_name, username, password, role) {
-    await pool.query('INSERT INTO users (full_name, username, password, role) VALUES ($1, $2, $3, $4)', [full_name, username, password, role]);
+async function newUserPost(fullname, username, password) {
+	await pool.query(
+		'INSERT INTO users (full_name, username, password) VALUES ($1, $2, $3)',
+		[fullname, username, password]
+	);
 }
 
-
+async function findByUsername(value) {
+	const { rows } = await pool.query(
+		'SELECT EXISTS (SELECT 1 FROM users WHERE username = $1) AS "exists"',
+		[value]
+	);
+	return rows[0].exists;
+}
 
 module.exports = {
-    newUserPost
-}
+	newUserPost,
+	findByUsername,
+};
