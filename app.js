9d807e900078433450aa313a18b01a271413cb9a
Shumaila <sayedshumaila27@gmail.com>
2025-07-31 21:16:17 +0530

    Integrate passport

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ require('dotenv').config();
 const path = require('node:path')
 const express = require('express');
 const app = express();
+
+const session = require('express-session');
+const passport = require('./config/passport');
+
 const signUpRouter = require('./routes/signUpRouter');
 const membershipRouter = require('./routes/membershipRouter');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: false }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
 	res.locals.currentUser = req.user;
--- db/queries.js
+++ db/queries.js
@@ -16,11 +16,27 @@ async function findByUsername(value) {
 }
 
 async function updateMembership(user) {
-	await pool.query("UPDATE users SET role = 'member' WHERE username = $1", [user.username]);
+	await pool.query("UPDATE users SET role = 'member' WHERE username = $1", [
+		user.username,
+	]);
+}
+
+async function getUserByName(username, password) {
+	const { rows } = await pool.query('SELECT * FROM users WHERE username = $1', [
+		username,
+	]);
+	return rows[0];
+}
+
+async function getUserById(id) {
+	const { rows } = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+	return rows[0];
 }
 
 module.exports = {
 	newUserPost,
 	findByUsername,
 	updateMembership,
+	getUserByName,
+	getUserById,
 };
